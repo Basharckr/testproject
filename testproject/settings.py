8c9339967ade7b393f26e51861ff8607a697e902
ameendeerom <ameen@deerom.com>
2021-09-13 12:16:32 +0530

    .env file added

--- testproject/settings.py
+++ testproject/settings.py
@@ -13,15 +13,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0j^_rdx+u)qe7dp!7t1e42fi0gs!qshroq_o2a-q%f38s#9-eq'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
